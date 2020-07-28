9218da6279f5bd1930da90634bdbbb282ade4413
mehdi maizi <mehdi.maizi@outlook.fr>
2020-07-28 15:11:56 +0100

    actualités covid

---
+++ Models/ActualiteCovid.cs
@@ -0,0 +1,19 @@
+namespace stopcovid.Models
+{
+    using System;
+   public class ActualiteCovid
+{
+public DateTime Date { get; set; }
+
+public string Actualitedujour { get; set; }
+
+
+}
+
+
+}
+
+
+
+
+
