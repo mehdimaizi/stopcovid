029a639f3dda7400b37c58b881e921b3fafeaeb3
Ilias Jennane <iliasj@microsoft.com>
2020-08-03 14:06:27 -0400

    fixed syntax issues

--- Models/ActualiteCovid.cs
+++ Models/ActualiteCovid.cs
@@ -1,14 +1,22 @@
+using System;
+using System.Text.Json.Serialization;
 namespace stopcovid.Models
 {
     using System;
-   public class ActualiteCovid
-{
-public DateTime Date { get; set; }
+    public class ActualiteCovid
+    {
+         
+        [JsonPropertyName("date")]
+        public DateTime Date { get; set; }
 
-public string Actualitedujour { get; set; }
+        [JsonPropertyName("info")]
+        public string Actualitedujour { get; set; }
+
+        [JsonPropertyName("source")]
+        public string Provenance { get; set; }
 
 
-}
+    }
 
 
 }
