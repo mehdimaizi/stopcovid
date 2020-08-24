a2aa066655539c8162df39c6fec34e79fe233522
Ilias Jennane <iliasj@microsoft.com>
2020-08-24 15:06:25 -0400

    ajout du tracage

--- Models/Ensemblecas.cs
+++ Models/Ensemblecas.cs
@@ -3,6 +3,7 @@ namespace stopcovid.Models
     using System;
     public class Ensemblecas
     {
+        public int RegionId { get; set; }
         public String Regions { get; set; }
         public int Cas { get; set; }
         
